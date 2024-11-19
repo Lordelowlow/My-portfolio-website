62ed0aee1a52c63561edf7ed2cdfc9986e93568d
Turner Osheen <osheen.turner@etu.umontpellier.fr>
2024-11-19 21:57:29 +1000

    Portrait mode added, basic responsive

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Portfolio from './components/Portfolio';
 import About from './components/About';
 import Resume from './components/Resume';
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,27 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Home() {
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
     useEffect(() => {
-        const script = document.createElement('script');
-        script.type = 'module';
-        script.src = 'https://unpkg.com/@splinetool/viewer@1.9.43/build/spline-viewer.js';
-        document.body.appendChild(script);
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 768);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
     }, []);
 
     return (
         <div>
-            <spline-viewer url="https://prod.spline.design/GvsgZL4oq7cauXNz/scene.splinecode"></spline-viewer>
+            {isMobile ? (
+                <spline-viewer key="mobile" url="https://prod.spline.design/VZtZ6j1iur7oC7ny/scene.splinecode"></spline-viewer>
+            ) : (
+                <spline-viewer key="desktop" url="https://prod.spline.design/GvsgZL4oq7cauXNz/scene.splinecode"></spline-viewer>
+            )}
         </div>
     );
 }
